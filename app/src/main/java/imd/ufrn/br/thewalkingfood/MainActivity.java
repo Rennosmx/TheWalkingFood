03b4584503e4aa9f6adc0b4f4028c3f5ac334ddf
Renno <rennosmx@hotmail.com>
2016-10-06 04:31:49 -0300

    Tela inicial carregando para Tela de Seleção de Perfil

--- app/src/main/java/imd/ufrn/br/thewalkingfood/MainActivity.java
+++ app/src/main/java/imd/ufrn/br/thewalkingfood/MainActivity.java
@@ -1,13 +1,60 @@
 package imd.ufrn.br.thewalkingfood;
 
+
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    protected ProgressBar barraProgresso;
+    private int tempoProgresso = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        barraProgresso = (ProgressBar)findViewById(R.id.barraDeProgresso);
+        carregar();
     }
+
+    //Simulando o carregamento da Tela de Perfil
+    public void carregar(){
+            final Thread thread = new Thread(){
+
+            @Override
+            public void run() {
+                Random rand = new Random();
+                try {
+                    while( tempoProgresso < barraProgresso.getMax() ){
+                        sleep(40);
+                        tempoProgresso += rand.nextInt(5) + 1;
+                        atualizarProgresso(tempoProgresso);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                progressoFinalizado();
+            }
+        };
+        thread.start();
+    }
+
+    public void atualizarProgresso(int valor){
+        barraProgresso.setProgress(valor);
+    }
+
+    public void progressoFinalizado(){
+        Intent intent = new Intent(MainActivity.this, SelecaoPerfilActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/imd/ufrn/br/thewalkingfood/SelecaoPerfilActivity.java
@@ -0,0 +1,13 @@
+package imd.ufrn.br.thewalkingfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelecaoPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selecao_perfil);
+    }
+}
