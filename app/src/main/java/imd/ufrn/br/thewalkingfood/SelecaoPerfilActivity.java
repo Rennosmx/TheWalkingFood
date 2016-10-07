5a443ce6d8de1f0cd2d5e15690bfe01905fcd79d
Renno <rennosmx@hotmail.com>
2016-10-07 04:06:35 -0300

    Criação de ImageButtons para encaminhar para telas de cadastro.

--- app/src/main/java/imd/ufrn/br/thewalkingfood/SelecaoPerfilActivity.java
+++ app/src/main/java/imd/ufrn/br/thewalkingfood/SelecaoPerfilActivity.java
@@ -1,13 +1,29 @@
 package imd.ufrn.br.thewalkingfood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.ImageButton;
 
 public class SelecaoPerfilActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selecao_perfil);
+
+    }
+
+    public void cadastroAmbulante(View view) {
+        Intent intent = new Intent(SelecaoPerfilActivity.this, CadastroAmbulanteActivity.class);
+        startActivity(intent);
+    }
+
+    public void cadastroConsumidor(View view) {
+        Intent intent = new Intent(SelecaoPerfilActivity.this, CadastroConsumidorActivity.class);
+        startActivity(intent);
     }
 }
