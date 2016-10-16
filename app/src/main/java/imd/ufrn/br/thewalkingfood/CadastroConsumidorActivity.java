1815aa13169b2cabbe4d1208482dc506c8eb5b8d
Renno <rennosmx@hotmail.com>
2016-10-15 21:33:29 -0300

    Padronizando imagens

--- app/src/main/java/imd/ufrn/br/thewalkingfood/CadastroConsumidorActivity.java
+++ app/src/main/java/imd/ufrn/br/thewalkingfood/CadastroConsumidorActivity.java
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+
 public class CadastroConsumidorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_consumidor);
+
     }
 
     public void finalizarCadastro(View view) {
--- app/src/main/java/imd/ufrn/br/thewalkingfood/SelecaoPerfilActivity.java
+++ app/src/main/java/imd/ufrn/br/thewalkingfood/SelecaoPerfilActivity.java
@@ -1,5 +1,4 @@
 package imd.ufrn.br.thewalkingfood;
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,7 +17,7 @@ public class SelecaoPerfilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         /*FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
-        */
+*/
         setContentView(R.layout.activity_selecao_perfil);
 
         rbVendedor = (RadioButton)findViewById(R.id.rBtnVendedor);
